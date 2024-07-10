11ce36c5539d4f130392e2df5e73636b275e3805
zacharygoulet <zachary.goulet9@gmail.com>
2024-07-09 22:03:29 -0400

    remove post example, use main example only

--- src/material.rs
+++ src/material.rs
@@ -9,12 +9,9 @@ pub(crate) const BAR_SHADER_HANDLE: Handle<Shader> = Handle::weak_from_u128(1302
 
 #[derive(Asset, AsBindGroup, TypePath, Debug, Clone)]
 pub struct StatusBarMaterial {
-    #[uniform(0)]
-    pub foreground_color: Color,
-    #[uniform(0)]
-    pub background_color: Color,
-    #[uniform(0)]
-    pub percent: f32,
+    #[uniform(0)] pub foreground_color: Color,
+    #[uniform(0)] pub background_color: Color,
+    #[uniform(0)] pub percent: f32,
 }
 
 impl Material for StatusBarMaterial {
